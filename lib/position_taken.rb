7b4bb444daf03d93f27fa7895bbee27ccf47d2b6
Rachel Wyatt <rw126@evansville.edu>
2018-07-22 13:14:33 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,5 @@
 def position_taken? (board, index)
-  if board[index] == " "
-    false
-  elsif board[index] == nil
+  if board[index] == " " || board[index] == "" || board[index] == nil
     false
   elsif board[index] == "X" || board[index] == "O"
     true
